9894dfec42ba31e86ddba2f74ac9f6defcd61323
ninajhun <ninajhun@berkeley.edu>
2021-06-18 16:59:35 -0700

    add data.js

---
+++ waste-ed/src/data.js
@@ -0,0 +1,39 @@
+let data = [
+  {
+    foodItem: "eggs",
+    servings: 2,
+    expiration: 2, // (days until expires),
+    img: "src-link",
+    status: "eaten",
+  },
+  {
+    foodItem: "lettuce",
+    servings: 3,
+    expiration: 2,
+    img: "src-link",
+    status: "uneaten",
+  },
+  {
+    foodItem: "milk ",
+    servings: 1,
+    expiration: 2,
+    img: "src-link",
+    status: "waste",
+  },
+];
+
+//loop thru pantry if pantry.status === "uneaten" render eaten component
+//or maybe filter return arr
+
+// var todos = [];
+
+// var previousTodosJSON = localStorage.getItem("javascript-local-storage");
+
+// if (previousTodosJSON !== null) {
+//   todos = JSON.parse(previousTodosJSON);
+// }
+
+// window.addEventListener("beforeunload", function (event) {
+//   var todosJSON = JSON.stringify(todos);
+//   localStorage.setItem("javascript-local-storage", todosJSON);
+// });
