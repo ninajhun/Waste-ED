d8a70e3200c002ff2f2e1266748ffaa5288f5ce2
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-06-19 20:32:23 +0000

    connected localStorage to wastedItem component

--- waste-ed/src/components/WastedItem.js
+++ waste-ed/src/components/WastedItem.js
@@ -1,33 +1,38 @@
 import React from 'react';
 import ItemCard from './ItemCard';
 
+const jsonData = localStorage.getItem("local-data");
+const items = JSON.parse(jsonData);
+
+
 class WastedItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: [
-        {
-          foodItem: "eggs",
-          servings: 2,
-          expiration: 1,
-          img: "https://toppng.com/uploads/preview/eggs-11528350931egreshkuji.png",
-          status: "eaten",
-        },
-        {
-          foodItem: "lettuce",
-          servings: 3,
-          expiration: 3,
-          img: "https://www.vhv.rs/dpng/d/88-883016_iceberg-lettuce-png-transparent-png.png",
-          status: "uneaten",
-        },
-        {
-          foodItem: "milk ",
-          servings: 1,
-          expiration: 6,
-          img: "https://toppng.com/uploads/preview/milk-png-115540200115wsmxoplcu.png",
-          status: "waste",
-        }
-      ]
+      items: items
+      // [
+      //   {
+      //     foodItem: "eggs",
+      //     servings: 2,
+      //     expiration: 1,
+      //     img: "https://toppng.com/uploads/preview/eggs-11528350931egreshkuji.png",
+      //     status: "eaten",
+      //   },
+      //   {
+      //     foodItem: "lettuce",
+      //     servings: 3,
+      //     expiration: 3,
+      //     img: "https://www.vhv.rs/dpng/d/88-883016_iceberg-lettuce-png-transparent-png.png",
+      //     status: "uneaten",
+      //   },
+      //   {
+      //     foodItem: "milk ",
+      //     servings: 1,
+      //     expiration: 6,
+      //     img: "https://toppng.com/uploads/preview/milk-png-115540200115wsmxoplcu.png",
+      //     status: "waste",
+      //   }
+      // ]
     }
   }
 
