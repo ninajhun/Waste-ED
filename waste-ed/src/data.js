fada4def0c6f969469457c6bf6240d288661f415
Nina Jhunjhnuwala <52934863+ninajhun@users.noreply.github.com>
2021-06-19 14:08:32 -0700

    Update data.js

--- waste-ed/src/data.js
+++ waste-ed/src/data.js
@@ -1,34 +1,7 @@
-let data = [
-  {
-    foodItem: "eggs",
-    servings: 2,
-    expiration: 2, // (days until expires),
-    img: "src-link",
-    status: "eaten",
-  },
-  {
-    foodItem: "lettuce",
-    servings: 3,
-    expiration: 2,
-    img: "src-link",
-    status: "uneaten",
-  },
-  {
-    foodItem: "milk ",
-    servings: 1,
-    expiration: 2,
-    img: "src-link",
-    status: "waste",
-  },
-];
-
-
-//loop thru pantry if pantry.status === "uneaten" render eaten component
-//or maybe filter return arr
-
-//can you see me?!
-
-// var todos = [];
+let data = {
+  food: [],
+  nextId: 1,
+};
 
 const previousData = localStorage.getItem("local-data");
 if (previousData !== null) {
@@ -42,3 +15,4 @@ function beforeUnload(event) {
 }
 
 export { data };
+
