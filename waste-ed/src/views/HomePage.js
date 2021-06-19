560339f20e65268430e1fa2c68eb6bb7bf337565
Angel Leung <angelhleung@gmail.com>
2021-06-18 20:17:21 -0700

    Added API key and function

--- waste-ed/src/views/HomePage.js
+++ waste-ed/src/views/HomePage.js
@@ -1,7 +1,16 @@
 import React from "react";
 import AddItemBar from "../components/AddItemBar";
+import fetchAPI from "../fetchAPI";
 
 export default function HomePage() {
+  const input = 'banana';
+  fetchAPI(`https://trackapi.nutritionix.com/v2/search/instant?branded=false&query=${input}`, input)
+    .then(data => {
+      console.log(data.common);
+      // data.common returns the array of 20 items
+      // data.common[0].food_name is the item name
+      // data.common[0].photo.thumb is the image
+    });
   return (
     <>
       <AddItemBar />
